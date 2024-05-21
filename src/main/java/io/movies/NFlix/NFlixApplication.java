52109ae5746a763407396472e30ed9a2fb5d3a48
Nestor_Damien <75420711+Nestor-19@users.noreply.github.com>
2024-05-21 19:15:40 -0400

    initial commit

---
+++ src/main/java/io/movies/NFlix/NFlixApplication.java
@@ -0,0 +1,13 @@
+package io.movies.NFlix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NFlixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NFlixApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/movies/NFlix/NFlixApplicationTests.java
@@ -0,0 +1,13 @@
+package io.movies.NFlix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NFlixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
