24f72320ba47915021f5bc737664c59dd49bb9cc
Nestor_Damien <75420711+Nestor-19@users.noreply.github.com>
2024-05-21 19:20:20 -0400

    Added first API endpoint for testing

--- src/main/java/io/movies/NFlix/NFlixApplication.java
+++ src/main/java/io/movies/NFlix/NFlixApplication.java
@@ -2,12 +2,21 @@ package io.movies.NFlix;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class NFlixApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(NFlixApplication.class, args);
 	}
+
+	@GetMapping("/")
+	public String apiRoot() {
+		return "Hi, this is my first Spring boot API endpoint";
+	}
+
 
 }
