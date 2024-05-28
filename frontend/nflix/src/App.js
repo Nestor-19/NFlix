ca58db70f142a2114d6e8569a8f0bf96f6ee03cc
Nestor_Damien <75420711+Nestor-19@users.noreply.github.com>
2024-05-28 18:46:21 -0400

    Updated axiosConfig and used axios to handle GET request from frontend to the backend

--- frontend/nflix/src/App.js
+++ frontend/nflix/src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
+import api from './api/axiosConfig';
 
 function App() {
+  const [movies, setMovies] = useState();
+
+  const getAllMovies = async () => {
+    try {
+      const response = await api.get("/api/v1/movies");
+      console.log(response.data);
+      if (response.status === 200) {
+        setMovies(response.data);
+      } else {
+        console.log('Unexpected status code:', response.status);
+      }
+    } catch(error) {
+      console.log('Fetch to get movies failed with error: ' + error);
+    }
+  }
+
+  useEffect(() => {
+    getAllMovies();
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
     </div>
   );
 }
