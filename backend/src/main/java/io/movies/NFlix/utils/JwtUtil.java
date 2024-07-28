7c25f88c443ff1c0f987d51623d9e9f3cf74203b
Nestor_Damien <75420711+Nestor-19@users.noreply.github.com>
2024-07-28 14:37:00 -0400

    Created a JwtUtil.java to handle generating a jwt token

---
+++ backend/src/main/java/io/movies/NFlix/utils/JwtUtil.java
@@ -0,0 +1,25 @@
+package io.movies.NFlix.utils;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+@Component
+public class JwtUtil {
+    @Autowired
+    private Environment environment;;
+
+    public String generateToken(String username) {
+        String SECRET_KEY = environment.getProperty("env.data.jwtSecretKey");
+        return Jwts.builder()
+                .setSubject(username)
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 10))
+                .signWith(SignatureAlgorithm.HS256, SECRET_KEY)
+                .compact();
+    }
+}
